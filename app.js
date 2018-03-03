1acd8a011a472c4d886ce449ecd7a1d8588fed48
Geoff <geoffrymichael@gmail.com>
2018-03-03 06:03:39 +0000

    Preparing for heroku launch. Move database url into an environmental variable

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ var campgroundRoutes    = require ("./routes/campgrounds");
 var indexRoutes         = require ("./routes/index");
 
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//Database link
+mongoose.connect(process.env.DATABASEURL);
+
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
